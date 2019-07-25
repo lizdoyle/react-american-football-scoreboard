cab32bf64c03123f75e293155a3605d474e04f9c
lizdoyle <lizdoyle1121@gmail.com>
2019-07-25 01:31:40 -0400

    close on timer

--- src/App.js
+++ src/App.js
@@ -7,10 +7,28 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(32); 
   const [awayScore, setAwayScore] = useState(32);
-  const [timer, setTimer] = useState(30, () => { 
-    return timer - 1
-  });
+  let [timer, setTimer] = useState(30);
   
+  
+// const newTimer = () => {
+//     return timer.map( setTimer(timer - 1) = () => { if(newTimer <timer) ? (timer - 1)})
+//   };
+
+ const newTimer = () => { 
+
+  if (timer === 0 && setTimer(timer + 1) > 0) {
+    timer = setInterval(setTimer, 1000)
+    return newTimer + 1;
+  };
+
+  
+
+  // console.log(newTimer);
+  
+};
+
+
+
 
   return (
     <div className="container">
@@ -23,7 +41,7 @@ function App() {
 
             <div className="home__score"> {homeScore} </div>
           </div>
-          <div className="timer">{timer}</div>
+          <div className="timer">00:{timer}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score"> {awayScore} </div>
@@ -42,7 +60,7 @@ function App() {
           <button onClick={() => [setAwayScore(awayScore + 3)]} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
         <div className="cdTimer">
-          <button onClick={() => [setTimer(timer)] } className="buttons">Countdown Timer</button>
+          <button onClick={newTimer} className="buttons">Countdown Timer</button>
           </div>
       </section>
     </div>
